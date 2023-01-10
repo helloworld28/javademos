c58e2e4e435efb784b6dd7f9b482f79edeafb0e2
powerjun <ohyeah28@yeah.net>
2023-01-10 22:24:15 +0800

    Add CoreJava Demos

---
+++ corejava-demo/src/main/java/com/powerjun/demos/corejava/io/MemMapDemo.java
@@ -0,0 +1,27 @@
+package com.powerjun.demos.corejava.io;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.file.OpenOption;
+import java.nio.file.Path;
+
+public class MemMapDemo {
+    public static void main(String[] args) throws IOException {
+        FileChannel fileChannel = FileChannel.open(Path.of("text.txt"));
+        MappedByteBuffer byteBuffer = fileChannel.map(FileChannel.MapMode.READ_ONLY, 0, fileChannel.size());
+
+        ByteBuffer byteBuffer1 = ByteBuffer.allocate(1024);
+        byteBuffer1.put("Hello".getBytes());
+        byteBuffer1.flip();
+        while (byteBuffer1.hasRemaining()){
+            System.out.println(byteBuffer1.get());
+        }
+
+        byteBuffer1.rewind();
+        byteBuffer1.put("12345".getBytes());
+
+
+    }
+}
